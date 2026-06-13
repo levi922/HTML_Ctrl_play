6d13117e0c80b67e1015d09b6688a5be94748367
levi922 <00001108006607sp@al.educacao.sp.gov.br>
2026-06-13 12:28:27 -0300

    Add files via upload

---
+++ Modulo_2/HTML/aula14_javascript/script.js
@@ -0,0 +1,6 @@
+function passeNormal(){
+    document.getElementById("historia").innerHTML = "Um passe mais casual para quem não quer gastar muito e quer trar bastante proveito do jogo"
+}
+function passePlus(){
+    document.getElementById("historia").innerHTML = "Um passe um pouco mais caro, muito mais recompensas e para quem busca evoluir rápido no jogo e aproveitar ao máximo tudo oque ele pode oferecer"
+}
