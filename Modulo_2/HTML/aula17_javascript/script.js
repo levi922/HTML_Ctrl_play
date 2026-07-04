185fbd1a0ed1e16ac03238d7c22abf829f948463
levi922 <00001108006607sp@al.educacao.sp.gov.br>
2026-07-04 12:28:20 -0300

    Add files via upload

---
+++ Modulo_2/HTML/aula17_javascript/script.js
@@ -0,0 +1,18 @@
+let display = document.getElementById("display");
+
+function adicionar(valor){
+    display.value += valor;
+}
+
+function limpar(){
+    display.value = "";
+}
+
+function calcular(){
+    try{
+        display.value = eval(display.value)
+    }
+    catch{
+        display.value = "Erro"
+    }
+}
